7a2eefe5a03814aaed456c61170b7d44987d53e7
Jadson Santos <jadson.santos@protonmail.com>
2022-01-30 17:01:16 -0300

    update

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 
 const router = new Router();
 
-router.get("/auth/authorize", async (ctx) => {
+router.get("/auth/authorize", (ctx) => {
 
     const client_id = ctx.request.url.searchParams.get("client_id");
     const redirect_url = ctx.request.url.searchParams.get("redirect_uri");
@@ -12,7 +12,48 @@ router.get("/auth/authorize", async (ctx) => {
     console.log("redirect_url", redirect_url)
     console.log("state", state)
 
-    await ctx.send({ root: `${Deno.cwd()}/public`,index: "index.html" });
+    ctx.response.body = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>HomeAssistant Skill Alexa</title>
+            <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+            <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+            <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+            <style>
+                input {
+                    margin-top: 10px;
+                    margin-bottom: 10px;
+                }
+            </style>
+        </head>
+        <body>
+            <div class="container">
+                <div class="card card-container">
+                    <form class="form">
+                        <input type="text" id="homeassistant-url" class="form-control" placeholder="Home Assistant Url" required autofocus>
+                        <button class="btn btn-lg btn-primary btn-block btn-signin" type="submit">Seguir</button>
+                    </form>
+                </div>
+            </div>
+
+            <script type="text/javascript">
+                document.getElementById('form').addEventListener("submit",(event)=> {
+                    event.preventDefault()
+
+                    const homeassistantUrl = document.getElementById('homeassistant-url').target.value
+                    const path = "/auth/authorize?client_id=${client_id}&redirect_uri=${redirect_url}"
+                    window.location.href= homeassistantUrl + path
+
+                })
+            </script>
+        </body>
+        </html>
+    `
 
 });
 
