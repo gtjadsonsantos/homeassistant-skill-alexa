0ae552998a7e0095889f77a417383b25d5488db8
Jadson Santos <jadson.santos@protonmail.com>
2022-01-30 16:20:32 -0300

    update

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 
 console.log("Listening on http://localhost:8000");
 serve((req) => {
-  return new Response("Hello World!", {
+  return new Response("Xablau!", {
     headers: { "content-type": "text/plain" },
   });
 });
