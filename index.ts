735844abf3851d21901b0500b9268bcc7e8714d4
Jadson Santos <jadson.santos@protonmail.com>
2022-01-30 16:36:31 -0300

    update

--- index.ts
+++ index.ts
@@ -8,8 +8,10 @@ router.get("/auth/authorize", (ctx) => {
     const redirect_url = ctx.request.url.searchParams.get("redirect_uri");
     const state = ctx.request.url.searchParams.get("state");
 
-    console.log( ctx.request.url.searchParams.keys())
-    ctx.response.body = ctx.request.url.searchParams.keys()
+    const params = new URLSearchParams([["a", "b"], ["c", "d"]]);
+    for (const key of params.keys()) {
+     console.log(key);
+    }    ctx.response.body = ctx.request.url.searchParams.keys()
 });
 
 router.get("/token", (ctx) => {
