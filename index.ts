0e6cc9aff5c1b4f6772b0181aa83ace115d8acb0
Jadson Santos <jadson.santos@protonmail.com>
2022-02-01 05:32:45 -0300

    update

--- index.ts
+++ index.ts
@@ -46,7 +46,7 @@ router.get("/auth/authorize", (ctx) => {
                     event.preventDefault()
 
                     const homeassistantUrl = document.getElementById('homeassistant-url').value
-                    const path = "/auth/authorize?client_id=${client_id}&redirect_uri=${redirect_url}&state=${state}"
+                    const path = "/auth/authorize?client_id=${client_id}&redirect_uri=${redirect_url}&state=${state}&homeassistant=" + homeassistantUrl
                     
                     window.location.href= homeassistantUrl + path
 
@@ -59,20 +59,27 @@ router.get("/auth/authorize", (ctx) => {
 
 
 
-router.post("/auth/token", (ctx) => {
-    const grant_type = ctx.request.url.searchParams.get("grant_type");
-    const code = ctx.request.url.searchParams.get("code");
-    const client_id = ctx.request.url.searchParams.get("client_id");
-    const client_secret = ctx.request.url.searchParams.get("client_secret");
-    
-    const redirect_url = ctx.request.url.searchParams.get("redirect_uri");
-    const state = ctx.request.url.searchParams.get("state");
+router.post("/auth/token", async (ctx) => {
 
-    console.log("client_id", client_id)
-    console.log("redirect_url", redirect_url)
-    console.log("state", state)
+    const homeassistant = ctx.request.url.searchParams.get("homeassistant");
 
+    const {
+        client_id,
+        code,
+        grant_type
+    } = await ctx.request.body().value 
 
+    console.log("client_id",client_id)
+    console.log("code",code)
+    console.log("grant_type",grant_type)
+    console.log("homeassistant",homeassistant)
+
+    //ctx.response.redirect(``)
+
+    //access_token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJjZjIxNjBhZmViOTk0OTkyYTkxODM5NGU1YmQ4YzU2YSIsImlhdCI6MTY0MzcwMzk5MCwiZXhwIjoxNjQzNzA1NzkwfQ.b1lfnu4Q8E_jagQE_6vioqxKUMTzyeqxFIsgKdoFEUA"
+    //expires_in: 1800
+    //refresh_token: "fb01a2514da8013f899e05befaeafa90ac5fb2628a99f6d3bd1591baf2b4ae07f4876816cd08bcafce6e30f7092ebcfbd1ad04898013359d5e6bbee9c6aaf3cf"
+    //token_type: "Bearer"
 
 });
   
