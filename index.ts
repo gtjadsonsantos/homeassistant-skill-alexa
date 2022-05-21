11c50ed83a9af8187e617d6f81e966643531d118
Jadson Santos <jadson44.santos@gmail.com>
2022-05-21 19:16:27 -0300

    update

--- index.ts
+++ index.ts
@@ -2,24 +2,20 @@ import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 import axiod from "https://deno.land/x/axiod/mod.ts";
 import { encodeUrl } from "https://deno.land/x/encodeurl/mod.ts";
 
-
-
 const app = new Application();
 const router = new Router();
 
 app.use(router.routes());
 app.use(router.allowedMethods());
 
+router.get("/auth/authorize", (ctx) => {
+  const client_id = "https://smarthome.deno.dev"; //ctx.request.url.searchParams.get("client_id");
+  const response_type = ctx.request.url.searchParams.get("response_type");
+  const state = ctx.request.url.searchParams.get("state");
+  const scope = ctx.request.url.searchParams.get("scope");
+  const redirect_uri = "https://smarthome.deno.dev/auth_callback"; //ctx.request.url.searchParams.get("redirect_uri");
 
-router.get("/auth/authorize",  (ctx) => {
-
-    const client_id = "https://smarthome.deno.dev" //ctx.request.url.searchParams.get("client_id");
-    const response_type = ctx.request.url.searchParams.get("response_type");
-    const state = ctx.request.url.searchParams.get("state");
-    const scope = ctx.request.url.searchParams.get("scope");
-    const redirect_uri = "https://smarthome.deno.dev/auth_callback" //ctx.request.url.searchParams.get("redirect_uri");
-
-    ctx.response.body = `
+  ctx.response.body = `
     <!DOCTYPE html>
     <html>
     <head>
@@ -45,32 +41,40 @@ router.get("/auth/authorize",  (ctx) => {
     `;
 });
 
-router.get("/auth_callback",  (ctx) => {
-    const code = ctx.request.url.searchParams.get("code");
-    const state = ctx.request.url.searchParams.get("state") as string;
+router.get("/auth_callback", (ctx) => {
+  const code = ctx.request.url.searchParams.get("code");
+  const state = ctx.request.url.searchParams.get("state") as string;
 
-    //console.log(encodeUrl(ctx.request.url.toString()))
-    //const state_parse: {hassUrl: string,clientId: string} = JSON.parse(atob(state))
+  //console.log(encodeUrl(ctx.request.url.toString()))
+  //const state_parse: {hassUrl: string,clientId: string} = JSON.parse(atob(state))
 
-    ctx.response.redirect(`https://pitangui.amazon.com/api/skill/link/M1S726D3FYBD5K?state=${state}&code=${code}`);
-    
-})
-
-router.post("/auth/token", async (ctx) => {
-    
-    const hass_url = "https://unisec.unicontrol.me"
-    const code = await ctx.request.url.searchParams.get("code") as string
-    const state = await ctx.request.url.searchParams.get("state") as string
-    const grant_type = await ctx.request.url.searchParams.get("grant_type") as string
-    const redirect_uri = await ctx.request.url.searchParams.get("redirect_uri") as string
-    const client_id = ctx.request.url.searchParams.get("client_id") as string
-
-    const { data } = await axiod.post(`${hass_url}/auth/token?grant_type=${grant_type}&code=${code}&client_id=${client_id}`)
-
-    ctx.response.body = data
-
+  ctx.response.redirect(
+    `https://pitangui.amazon.com/api/skill/link/M1S726D3FYBD5K?state=${state}&code=${code}`
+  );
 });
 
+router.post("/auth/token", async (ctx) => {
+  const hass_url = "https://unisec.unicontrol.me";
+  const code = ctx.request.url.searchParams.get("code") as string;
+  const state = ctx.request.url.searchParams.get("state") as string;
+  const grant_type = ctx.request.url.searchParams.get("grant_type") as string;
+  const redirect_uri = ctx.request.url.searchParams.get("redirect_uri") as string;
+  const client_id = ctx.request.url.searchParams.get("client_id") as string;
 
-app.addEventListener( "listen", () => console.log("Listening on https://smarthome.unicontrol.me"));
-await app.listen({ port: 8080 })
+  const { data } = await axiod.post(
+    `${hass_url}/auth/token`,
+    new URLSearchParams({grant_type,code,client_id}),
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }
+  );
+
+  ctx.response.body = data;
+});
+
+app.addEventListener("listen", () =>
+  console.log("Listening on https://smarthome.unicontrol.me")
+);
+await app.listen({ port: 8080 });
