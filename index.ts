73962fdf8d3cee2a9544215fe76e1277e756fdf2
Jadson Santos <jadson44.santos@gmail.com>
2022-05-27 16:16:18 -0300

    update

--- index.ts
+++ index.ts
@@ -71,8 +71,6 @@ router.post("/auth/token", async (ctx) => {
 
 router.post("/debug",  async  (ctx) => {
 
-  console.log(ctx.request.url.searchParams.toString())
-
   const data = await ctx.request.body({type: "json"}).value
 
   console.log(data.directive.header.correlationToken)
