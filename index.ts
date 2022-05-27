ed7bb4848ae6720c90948c0a5d3a3b31a527f75d
Jadson Santos <jadson44.santos@gmail.com>
2022-05-27 14:23:22 -0300

    udpate

--- index.ts
+++ index.ts
@@ -73,7 +73,8 @@ router.post("/debug",  async  (ctx) => {
 
   console.log(ctx.request.url.searchParams.toString())
 
-  console.log(await ctx.request.body().value)
+  const data = await ctx.request.body({type: "json"}).value
+console.log(data.directive.endpoint.scope.token)
 
   ctx.response.status = 200
 });
